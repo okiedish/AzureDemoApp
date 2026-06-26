b774a6524663d884a77cb57da7595d1a0948968f
okiedish <161029692+okiedish@users.noreply.github.com>
2026-06-26 23:38:30 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello disha how are you?? , this is a maven project for devops lab external" );
+        System.out.println( "Hello disha how are you?? , this is a maven project for devops lab external hehe" );
     }
 }
